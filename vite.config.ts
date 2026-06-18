e9da2b5817c5b7d0d082a2dc147add178207ff97
Marcus Hawkins <73599308+marcusehh@users.noreply.github.com>
2026-06-18 14:38:00 +0100

    Finish website

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/Kingstone_Investments/',
+  base: '/',
   plugins: [react()],
   server: { port: process.env.PORT ? Number(process.env.PORT) : 5173 }
 });
